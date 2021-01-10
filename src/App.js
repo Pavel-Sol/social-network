c985464943b4cc3c899cbf33df621ebc07162e74
pavel_sol <pavel.solov@inbox.ru>
2021-01-10 19:00:40 +0400

    добавил компонент 'dialogs'

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-// import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Link,
+  Route
+} from 'react-router-dom'
+
 import './App.css';
 import Header from './components/Header/Header'
 import Navbar from './components/Navbar/Navbar'
 import Profile from './components/Profile/Profile'
+// import Dialogs from './components/Dialogs/Dialogs'
 
 function App() {
   return ( 
     <div className= 'app-wrapper' >
     <Header />
     <Navbar />
-    <Profile />
+    <div className= 'app-wrapper-content'>
+      <Profile />
+       {/* <Dialogs /> */}
+    </div>
    </div>
   );
 }
---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,9 @@
+import s from './Dialogs.module.css'
+
+function Dialogs() {
+    return (
+        <h1>Dialogs</h1>
+    )
+}
+
+export default Dialogs
