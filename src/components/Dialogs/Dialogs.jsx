eee68d22426d03e673a2b3eed4bdd3f0de6b1104
pavel_sol <pavel.solov@inbox.ru>
2021-01-12 16:06:07 +0400

    в диалогах каждого юзера обернул в Navlink

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom'
 import s from './Dialogs.module.css'
 
 function Dialogs() {
@@ -5,13 +6,13 @@ function Dialogs() {
         <div className={s.dialogsWrap}>
             <div className={s.dialogsNames}>
                 <div className={s.dialogsName}>
-                    Victor
+                    <NavLink to='/dialogs/1'>Victor</NavLink>
                 </div>
                 <div className={s.dialogsName}>
-                    Sergey
+                    <NavLink to='/dialogs/2'>Sergey</NavLink>
                 </div>
                 <div className={s.dialogsName}>
-                    Marina
+                    <NavLink to='/dialogs/3'>Marina</NavLink>
                 </div>
             </div>
             <div className={s.messages}>
