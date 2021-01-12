eb3ebaec83cc89720db933e141e285f9e0e019d6
pavel_sol <pavel.solov@inbox.ru>
2021-01-12 15:38:57 +0400

    сверстал страницу диалогов u-21

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,7 +2,27 @@ import s from './Dialogs.module.css'
 
 function Dialogs() {
     return (
-        <h1>Dialogs</h1>
+        <div className={s.dialogsWrap}>
+            <div className={s.dialogsNames}>
+                <div className={s.dialogsName}>
+                    Victor
+                </div>
+                <div className={s.dialogsName}>
+                    Sergey
+                </div>
+                <div className={s.dialogsName}>
+                    Marina
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    Hi, how are you
+                </div>
+                <div className={s.message}>
+                    What is your name
+                </div>
+            </div>
+        </div>
     )
 }
 
