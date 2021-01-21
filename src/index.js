fb3e6f6e086747ca799991376766c902df2099e3
pavel_sol <pavel.solov@inbox.ru>
2021-01-21 20:08:52 +0400

    unit-35  callback, subscribe, observer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+import {addPost, updateNewPostText, subscribe} from './redux/state'
+
 import reportWebVitals from './reportWebVitals';
 import state from './redux/state'
-import {rerenderEntireTree} from './render'
+
+
+
+let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+      <React.StrictMode>
+        <App state={state} addPost= {addPost} updateNewPostText={updateNewPostText}/>
+      </React.StrictMode>,
+      document.getElementById('root')
+    );
+  }
 
 rerenderEntireTree (state)
 
+subscribe(rerenderEntireTree)
 
 reportWebVitals();
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import {rerenderEntireTree} from './../render'
+let rerenderEntireTree = () => {
+  console.log('state changed')
+}
 
 let state = {
     profilePage: {
@@ -41,5 +43,9 @@ export let updateNewPostText = (newText) => {
   state.profilePage.newPostText = newText
   rerenderEntireTree(state)
 }  
+
+export const subscribe = (observer) => {
+  rerenderEntireTree = observer
+}
 
 export default state
